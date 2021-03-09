3974423e53458a88e739c067279d9603e6cc6964
sinsang <sw980321@naver.com>
2021-03-09 15:30:22 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/list", (req, res) => {
+  res.send("무야호~");
+});
+
 module.exports = router;
