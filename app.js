5914f8aad222855649a0af830cc6e7239f81c0bf
sinsang <sw980321@naver.com>
2021-03-09 14:29:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.port;
+const port = process.env.PORT;
 
 app.use(express.static(__dirname + "/"));
 app.set("views", "views");
