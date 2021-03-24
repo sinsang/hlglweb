b44fd86a81221e13771bc05b377df225aab7a6f9
sinsang <sw980321@naver.com>
2021-03-24 17:44:43 +0900

    update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'hamburgers-master')));
+app.use(express.static(path.join(__dirname, 'hamburgers-master')));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // sessions
